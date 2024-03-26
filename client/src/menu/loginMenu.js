a20f4b2610674606143ca21666ce4482424493fa
antoine.gaienier <antoine.gaienier.etu@univ-lille.fr>
2024-03-26 14:44:20 +0100

    deconnexion

--- client/src/menu/loginMenu.js
+++ client/src/menu/loginMenu.js
@@ -11,32 +11,45 @@ export default class LoginMenu {
 			socket.emit('login', { login, password });
 		});
 	}
+
 	static setSignin(menu, socket) {
 		$('form', menu).on('submit', event => {
 			event.preventDefault();
 			const login = $('input[name=login]', menu).val();
 			const password = $('input[name=password]', menu).val();
 			const password2 = $('input[name=password2]', menu).val();
-			const question = $('input[name=question]', menu).val();
+			const recovery_phrase = $('input[name=question]', menu).val();
 			const reponse = $('input[name=reponse]', menu).val();
-			$('input[name=login]', menu).val('');
-			$('input[name=password]', menu).val('');
-			$('input[name=password2]', menu).val('');
-			$('input[name=question]', menu).val('');
-			$('input[name=reponse]', menu).val('');
-			socket.emit('signin', { login, password, password2, question, reponse });
+
+			if (password != password2) {
+				alert('Les mots de passe ne correspondent pas');
+				return;
+			} else {
+				$('input[name=login]', menu).val('');
+				$('input[name=password]', menu).val('');
+				$('input[name=password2]', menu).val('');
+				$('input[name=question]', menu).val('');
+				$('input[name=reponse]', menu).val('');
+				socket.emit('signin', {
+					login,
+					password,
+					recovery_phrase,
+					reponse,
+				});
+			}
 		});
 	}
+
 	static setMdp_oublie(menu, socket) {
 		$('form', menu).on('submit', event => {
 			event.preventDefault();
 			const login = $('input[name=login]', menu).val();
-			const question = $('input[name=question]', menu).val();
+			const recovery_phrase = $('input[name=question]', menu).val();
 			const reponse = $('input[name=reponse]', menu).val();
 			$('input[name=login]', menu).val('');
 			$('input[name=question]', menu).val('');
 			$('input[name=reponse]', menu).val('');
-			socket.emit('mdp_oublie', { login, question, reponse });
+			socket.emit('mdp_oublie', { login, recovery_phrase, reponse });
 		});
 	}
 	static resetPassword(menu, socket) {
@@ -47,6 +60,13 @@ export default class LoginMenu {
 			$('input[name=password]', menu).val('');
 			$('input[name=password2]', menu).val('');
 			socket.emit('resetPassword', { password, password2 });
+		});
+	}
+
+	static setLogout(menu, socket, user) {
+		$('.deconnexion', menu).on('click', event => {
+			event.preventDefault();
+			socket.emit('logout' , user);
 		});
 	}
 }
