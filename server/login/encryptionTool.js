2740071f100f31600ee877ea93d838f28ee0450a
antoine.gaienier <antoine.gaienier.etu@univ-lille.fr>
2024-04-02 10:08:42 +0200

    crytage ~~~~

--- server/login/encryptionTool.js
+++ server/login/encryptionTool.js
@@ -1,8 +1,19 @@
-//import CryptoJS from 'crypto-js';
+//import require from 'require';
 
-/*export default function encryptionTool(password) {
-	const salt = 'HeavenMustFall';
-	const combinedString = password + salt;
-	const hash = CryptoJS.MD5(combinedString);
-	return hash;
-}*/
+export default function encryptionTool(password) {
+	const crypto = require('crypto');
+
+	var text = '';
+
+	// On définit notre algorithme de cryptage
+	var algorithm = 'aes256';
+	var password = 'l5JmP+G0/1zB%;r8B8?2?2pcqGcL^3';
+	// On crypte notre texte
+	var cipher = crypto.createCipher(algorithm, password);
+	var crypted = cipher.update(text, 'utf8', 'hex');
+	crypted += cipher.final('hex');
+	// On décrypte notre texte
+	var decipher = crypto.createDecipher(algorithm, password);
+	var dec = decipher.update(crypted, 'hex', 'utf8');
+	dec += decipher.final('utf8');
+}
